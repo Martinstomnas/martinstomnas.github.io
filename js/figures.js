2eeb2c2bb3927e78db72068cedf2daad24caac1d
clovisbonja <clovishb@student.uia.no>
2023-11-21 14:20:15 +0100

    Update figures.js

--- js/figures.js
+++ js/figures.js
@@ -1,3 +1,7 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+
 //fuksjon for rektangel
 function tegnRektangel (height, width, x, y, mode, color) {
     ctx.fillstyle = color;
