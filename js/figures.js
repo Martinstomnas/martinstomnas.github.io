53cf0cc528308dee4e596e655182cc97dab9db82
ElineWi <elinewi@uia.no>
2023-11-21 14:27:00 +0100

    Update figures.js

--- js/figures.js
+++ js/figures.js
@@ -1,29 +1,21 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+// Canvas funksjoner 
 
-//fuksjon for rektangel
-function tegnRektangel (height, width, x, y, mode, color) {
-    ctx.fillstyle = color;
-    if (mode === "outline") {
-        // set the outline color
-        ctx.strokeStyle = color;
-        // Draw the rectangles outline
-        ctx.strokeRect(x, y, width, height);
-        // Draw the filled rectangle
-        ctx.fillRect(x, y, width, height);
-    }
-}
+//funksjon for rektangel
+Const rectangel = function(left, top, width, height, mode, color) {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
 
-//funksjon for sirkel
-circle = function (radius, mode, farge, x, y) {
-    ctx.fillStyle = color;
+    ctx.fillStyle = color; 
+    ctx.fillRect(left, top, width, height); 
+};
+
+//funksjon for sirkel 
+Const circle = function(centerleft, centertop, radius, mode, color) {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = color; 
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2, true); //outer circle
-    if (mode === "solid") {
-        ctx.fill()
-    } else {
-        ctx.stroke()
-    }
-}
-
-
+    ctx.ellipse(centerleft, centertop, radius, radius, color); 
+    ctx.fill();
+};
