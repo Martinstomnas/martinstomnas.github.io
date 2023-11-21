d9d5db75d11ad42eed87e1126774e54f6c64548a
clovisbonja <clovishb@student.uia.no>
2023-11-21 13:33:20 +0100

    Update figures.js

--- js/figures.js
+++ js/figures.js
@@ -1,1 +1,26 @@
+//fuksjon for rektangel
+function tegnRektangel (height, width, x, y, mode, color) {
+    ctx.fillstyle = color;
+    if (mode === "outline") {
+        // set the outline color
+        ctx.strokeStyle = color;
+        // Draw the rectangles outline
+        ctx.strokeRect(x, y, width, height);
+        // Draw the filled rectangle
+        ctx.fillRect(x, y, width, height);
+    }
+}
+
+//funksjon for sirkel
+circle = function (radius, mode, farge, x, y) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, true); //outer circle
+    if (mode === "solid") {
+        ctx.fill()
+    } else {
+        ctx.stroke()
+    }
+}
+
 
