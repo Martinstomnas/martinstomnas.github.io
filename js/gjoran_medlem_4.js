c9d31d45808108f4b39ccd95cbfb2da0e3316c27
Gjøran <gjoransr@uia.no>
2023-11-22 15:20:26 +0000

    "Lagde både SVG og CanvasAPI tegning. Endret personlig HTML kode"

---
+++ js/gjoran_medlem_4.js
@@ -0,0 +1,67 @@
+window.onload = function() {
+    setBackgroundColor("rgb(234, 190, 69)") // 
+
+    rectangleCanvas(0, 0, 500, 100, "solid", "rgb(233, 220, 125)") /// Himmel
+    circleCanvas(150, 325, 100, "solid", "rgb(236, 104, 58)") /// Sol 
+    rectangleCanvas(0, 300, 500, 500, "solid", "rgb(94, 195, 218)") 
+    circleCanvas(450, 360, 105, "solid", "rgb(221, 121, 100)") 
+    circleCanvas(450, 400, 95, "solid", "rgb(241, 161, 100)") 
+    circleCanvas(375, 400, 85, "solid", "rgb(231, 141, 100)") 
+    rectangleCanvas(0, 375, 500, 500, "solid", "rgb(74, 160, 201)")
+    circleCanvas(15, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(35, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(65, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(95, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(125, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(155, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(185, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(215, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(245, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(275, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(305, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(335, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(400, 400, 60, "solid", "rgb(231, 171, 100)")
+    circleCanvas(465, 390, 70, "solid", "rgb(251, 141, 100)")
+    rectangleCanvas(0, 390, 500, 500, "solid", "rgb(74, 160, 201)")
+    circleCanvas(345, 400, 15, "solid", "rgb(74, 160, 201)")
+    circleCanvas(370, 400, 15, "solid", "rgb(74, 160, 201)")
+    circleCanvas(395, 400, 15, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(420, 400, 15, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(445, 400, 15, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(470, 400, 15, "solid", "rgb(74, 160, 201)") 
+    circleCanvas(495, 400, 15, "solid", "rgb(74, 160, 201)")
+}
+    document.addEventListener("DOMContentLoaded", function() {
+        rectangleSVG(0, 0, 500, 500, "solid", "rgb(234, 190, 69)")
+        
+    rectangleSVG(0, 0, 500, 100, "solid", "rgb(233, 220, 125)")
+    circleSVG(150, 325, 100, "solid", "rgb(236, 104, 58)")
+    rectangleSVG(0, 300, 500, 500, "solid", "rgb(94, 195, 218)") // Sjø
+    circleSVG(450, 360, 105, "solid", "rgb(221, 121, 100)") 
+    circleSVG(450, 400, 95, "solid", "rgb(241, 161, 100)") 
+    circleSVG(375, 400, 85, "solid", "rgb(231, 141, 100)") 
+    rectangleSVG(0, 375, 500, 500, "solid", "rgb(74, 160, 201)") // Sjø  
+    circleSVG(15, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleSVG(35, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleSVG(65, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleSVG(95, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleSVG(125, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleSVG(155, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleSVG(185, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleSVG(215, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleSVG(245, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleSVG(275, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleSVG(305, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleSVG(335, 380, 30, "solid", "rgb(74, 160, 201)") 
+    circleSVG(400, 400, 60, "solid", "rgb(231, 171, 100)")
+    circleSVG(465, 390, 70, "solid", "rgb(251, 141, 100)")
+    rectangleSVG(0, 390, 500, 500, "solid", "rgb(74, 160, 201)")
+    circleSVG(345, 400, 15, "solid", "rgb(74, 160, 201)")
+    circleSVG(370, 400, 15, "solid", "rgb(74, 160, 201)")
+    circleSVG(395, 400, 15, "solid", "rgb(74, 160, 201)") 
+    circleSVG(420, 400, 15, "solid", "rgb(74, 160, 201)") 
+    circleSVG(445, 400, 15, "solid", "rgb(74, 160, 201)") 
+    circleSVG(470, 400, 15, "solid", "rgb(74, 160, 201)") 
+    circleSVG(495, 400, 15, "solid", "rgb(74, 160, 201)")
+    
+});
