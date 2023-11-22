1805d335ae1eed9a2500b3ab104fd303656bbdc0
clovisbonja <clovishb@student.uia.no>
2023-11-22 14:33:58 +0100

    Update clovis_medlem_6.js

--- js/clovis_medlem_6.js
+++ js/clovis_medlem_6.js
@@ -1,2 +1,23 @@
-circle(20,"solid","blue", 100, 100)
-tegnRektangel(200, 100, 20, 40, "solid", "blue")
+window.onload = function() {
+    setBackgroundColor("rgb(151, 210, 240)") // himmel
+
+
+    circleCanvas(350, 150, 100, "solid", "yellow") // sol
+    circleCanvas(100, 75, 25, "solid", "white") // sky
+    circleCanvas(120, 100, 25,"solid", "white") //sky
+    circleCanvas(130, 75, 25, "solid", "white") // sky
+    circleCanvas(140, 100, 25, "white") // sky
+    rectangleCanvas(0, 410, 500, 90, "solid", "rgb(184, 146, 70)") // sand
+    rectangleCanvas(0, 340, 500, 70, "solid", "rgb(237, 192, 102)") // sand
+    rectangleCanvas(0, 280, 500, 60, "solid", "rgb(109, 166, 227") // vann
+    rectangleCanvas(0, 240, 500, 40, "solid", "rgb(71, 151, 237)") // vann
+    rectangleCanvas(0, 220, 500, 20, "solid", "rgb(57, 135, 230)") // vann
+
+};
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    circleSVG(50, 50, 30, "someMode", "red");
+    circleSVG(100, 10, 30, "someMode", "red");
+  
+  });
